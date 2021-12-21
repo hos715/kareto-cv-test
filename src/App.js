fb9613becb5bf8e80cd3e8b53572fc3dc1bb5709
HOSein KH <hkh659@gmail.com>
2021-12-21 15:59:37 +0330

    add dictionary search form with pretty styles :)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 function App() {
   return (
-    <h1>
-      Hello Kareto
-    </h1>
+    <>
+      <div className="c-app">
+        <div className="c-app__wrapper">
+          <div className="c-app__header">
+            <img
+              className="c-app--image"
+              src="assets/images/logo.png"
+              width="125"
+              height="125"
+              alt="logo"
+            />
+            <h1 className="c-app__title">Hello Kareto</h1>
+          </div>
+          <form className="c-app__search-form">
+            <input
+              type="text"
+              className="c-app__search-form--input"
+              placeholder="the word goes here"
+            />
+            <input
+              type="submit"
+              value="search"
+              className="c-btn c-app__search-form--button"
+            />
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
